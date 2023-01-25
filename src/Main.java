64c84dc431d80559c4365f299439d3a3bf541514
Amilenkof <f.o.g@list.ru>
2023-01-25 17:12:38 +0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+     /*   task2();*/
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        /*Задача 1
+        Объявите переменные типа int, byte, short, long, float, double.
+        Название переменных может быть любым, но если состоит из двух слов и более, должно соответствовать правилу camelCase.
+        Выведите в консоль значение каждой переменной в формате «Значение переменной … с типом … равно …».// */
+        int a= 129;
+        System.out.println("Значение переменной а с типом int равно " +a);
+        byte b=12;
+        System.out.println("Значение переменной b с типом byte равно " +b);
+        short c= 353;
+        System.out.println("Значение переменной c с типом short равно " +c);
+        long d =12312312412L;
+        System.out.println("Значение переменной d с типом long равно " +d);
+        float e= 1213.4124f;
+        System.out.println("Значение переменной e с типом float равно " +e);
+        double f=1231.4121313121412;
+        System.out.println("Значение переменной f с типом double равно " +f);
+
+    }}
+/*
+    public static void task2 () {
+        System.out.println("Задача 2");
+        ... // Пишем код для задачи 2
+    }
+}*/
